d12e347c57912198e4b199bad74b7229fd43d6ac
ComeBackCity <50582070+ComeBackCity@users.noreply.github.com>
2021-04-15 19:32:35 +0600

    All tests done. Deployment done

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res)=>{
-    res.send({
-        to: "Sahil",
-        message: "Json testing"
-    })
+    res.send("CSE-408 backend")
 })
 
 app.listen(port, ()=> {
