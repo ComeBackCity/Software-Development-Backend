3819eb1a77223d5c9bb20e08d846bffdbe98581e
ComeBackCity <50582070+ComeBackCity@users.noreply.github.com>
2021-06-19 16:51:54 +0600

    Updated some files with prettierc

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const chalk = require("chalk")
-const mongodb = require("mongodb")
-const database = require('./src/utils/database/database')
-const bodyParser = require("body-parser");
-const storage = require('./src/utils/storage/storage')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const mongodb = require('mongodb');
+const database = require('./src/utils/database/database');
+const bodyParser = require('body-parser');
+const storage = require('./src/utils/storage/storage');
 
-const officerRouter = require('./src/routes/officer')
-const miscRouter = require('./src/routes/misc')
-const publicRouter = require('./src/routes/public')
+const officerRouter = require('./src/routes/officer');
+const miscRouter = require('./src/routes/misc');
+const publicRouter = require('./src/routes/public');
 
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(storage)
-app.use(officerRouter)
-app.use(miscRouter)
-app.use(publicRouter)
+app.use(storage);
+app.use(officerRouter);
+app.use(miscRouter);
+app.use(publicRouter);
 
 app.get('/', async (req, res) => {
-    res.send("CSE-408 backend")
-})
+	res.send('CSE-408 backend');
+});
 
 app.listen(port, () => {
-    console.log(chalk.keyword("green")("Server is up on port " + port));
-})
+	console.log(chalk.keyword('green')('Server is up on port ' + port));
+});
 
-module.exports = app
+module.exports = app;
