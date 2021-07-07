3602a2363a7c0ff65533fbecd30be93b3452bbd8
ComeBackCity <50582070+ComeBackCity@users.noreply.github.com>
2021-07-07 07:01:33 +0600

    Added update to gd

--- src/functions/gd/updateGD.js
+++ src/functions/gd/updateGD.js
@@ -1,8 +1,8 @@
-const caseModel = require('../../models/cases/case');
+const gdModel = require('../../models/gd/gd');
 const status_codes = require('../../utils/status_codes');
 
-const updateCase = async (req, res) => {
-	caseModel
+const assignOfficer = async (req, res) => {
+	gdModel
 		.findOneAndUpdate(
 			{
 				_id: req.query._id
@@ -14,7 +14,7 @@ const updateCase = async (req, res) => {
 		)
 		.then(r => {
 			return res.status(status_codes.SUCCESS).json({
-				message: 'Successfully updated the case'
+				message: 'Successfully updated the GD'
 			});
 		})
 		.catch(err => {
@@ -24,4 +24,4 @@ const updateCase = async (req, res) => {
 		});
 };
 
-module.exports = updateCase;
+module.exports = assignOfficer;
