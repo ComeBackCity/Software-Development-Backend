519be72633d32670fd2ad0deb74851b79add5f2a
ComeBackCity <50582070+ComeBackCity@users.noreply.github.com>
2021-07-28 02:02:09 +0600

    Updated thana add

--- src/functions/misc/thana.js
+++ src/functions/misc/thana.js
@@ -1,7 +1,16 @@
 const thanaModel = require('../../models/misc/thana');
 
 const createThana = async (req, res) => {
-	await thanaModel.insertMany(req.body);
+	await thanaModel
+		.insertMany(req.body)
+		.then(r => {
+			return res.json({
+				message: 'Success'
+			});
+		})
+		.catch(err => {
+			return res.json(err);
+		});
 };
 
 module.exports = createThana;
